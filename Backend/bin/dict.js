aa8e3190c77bd32d5d9c05abb81f4da29bfaf7d2
chan18 <chan18@users.noreply.github.com>
2020-02-29 19:48:04 +0530

    removed test inquirer code

--- Backend/bin/dict.js
+++ Backend/bin/dict.js
@@ -1,31 +1,4 @@
 #!/usr/bin/env node
 const inquirer = require('inquirer');
 
-const questions = [
-  {
-    name: 'username',
-    type: 'input',
-    message: 'Enter your GitHub username or e-mail address:',
-    validate: function( value ) {
-      if (value.length) {
-        return true;
-      } else {
-        return 'Please enter your username or e-mail address.';
-      }
-    }
-  },
-  {
-    name: 'password',
-    type: 'password',
-    message: 'Enter your password:',
-    validate: function(value) {
-      if (value.length) {
-        return true;
-      } else {
-        return 'Please enter your password.';
-      }
-    }
-  }
-];
 
-inquirer.prompt(questions);
