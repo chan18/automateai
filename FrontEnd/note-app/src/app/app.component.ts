0f7c68750bc9210fa77bf94076293be3baf02438
chan18 <chan18@users.noreply.github.com>
2020-03-03 03:19:01 +0530

    style fix

--- FrontEnd/note-app/src/app/app.component.ts
+++ FrontEnd/note-app/src/app/app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
-import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import { Component, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnDestroy {
   opened: boolean = true;
   list: Array<number> = [1, 2, 3,1, 2, 3,1, 2, 3,1, 2, 3,1, 2, 3,1, 2, 3,1, 2, 3];
-
   blured = false
   focused = false
-
   title = 'note-app';
 
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
