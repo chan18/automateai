4248d6abbc4cb72e674adec6a823ed60871352f0
chan18 <chan18@users.noreply.github.com>
2020-03-02 21:17:10 +0530

    added note-app, with angular material

---
+++ FrontEnd/note-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+  ],
+  entryComponents: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
